3945dd5b7cea79d1662110d80f3ba6bf219d4f95
sofjones <sofiaj611@gmail.com>
2022-05-03 21:52:04 -0700

    start

---
+++ vid.svelte
@@ -0,0 +1,60 @@
+<script>
+	import VideoPlayer from './video-player.svelte';
+	import Intro from './intro.svelte'
+	
+	const videoURL = 'https://ia601602.us.archive.org/11/items/Rick_Astley_Never_Gonna_Give_You_Up/Rick_Astley_Never_Gonna_Give_You_Up.mp4'
+	let time = 0;
+	let play = true;
+	let sound = true;
+	const name = 'Sofia Jones';
+	
+	function playVid(){
+		let vid = document.getElementById('vid');
+		if(play){
+			vid.pause();
+			play = false;
+		}
+		else{
+			vid.play();
+			play = true;
+		}
+	}
+	
+	function muteVid(){
+	let vid = document.getElementById('vid');
+	if(!vid.muted){
+		vid.muted = true;
+		sound = false;
+	}
+	else{
+		vid.muted = false;
+	}
+	}
+
+</script>
+<div>
+	<button on:click={playVid}>
+		Play
+	</button>
+	<button on:click={muteVid}>Mute</button>
+	<video
+		id='vid'
+		src={videoURL}
+		type="video/mp4"
+		autoplay=true
+		bind:currentTime={time}>
+		<track kind="captions">
+	</video>
+</div>
+
+<h1>
+	Coding Exercise: Rickroll Me
+</h1>
+
+{#if name}
+	<h2>
+		Submitted by: {name}
+	</h2>
+{/if}
+
+<Intro />
